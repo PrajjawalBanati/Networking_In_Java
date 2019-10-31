05a44ebe030046487ac5752091b0cf41f3222cd3
500060722 <500060722@stu.upes.ac.in>
2019-10-31 14:53:10 +0530

    Added TcpClient.java

---
+++ 8. Two Way Communication using TCP/TcpClient.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+public class TcpClient{
+	public static void main(String[] args) throws Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		Socket s=new Socket("localhost",5656);
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+		DataInputStream dis=new DataInputStream(s.getInputStream());
+		while(true)
+		{
+			System.out.println("Write Your message");
+			String str=scan.nextLine();
+			dout.writeUTF(str);
+			dout.flush();
+			if(str.equals("bye"))
+			{
+				dout.close();
+				s.close();
+				break;
+			}
+			String str1=(String)dis.readUTF();
+			System.out.println("Server Messaged--->"+str1);
+			
+			
+		}
+	}
+}
